7f6557fb967debe59f01428f357a302369cb2c1f
Diego Gonzalez <diego.gonzalez.glez91@gmail.com>
2020-05-06 21:44:53 -0500

    Se crea el controlador, el modelo y la migracion para guardar las respuestas de la encuesta

---
+++ app/Http/Controllers/GuardarRespuesta/GuardarRespuestaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\GuardarRespuesta;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GuardarRespuestaController extends Controller
+{
+    //
+}
